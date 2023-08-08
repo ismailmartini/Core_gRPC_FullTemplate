8f66c615f27132a010f62918d6f605d144473bad
imardinbr <ismail.mardin@brmagazacilik.com.tr>
2023-08-08 17:51:22 +0300

    client message update

--- Core_gRPC_UnaryTemplate_Client/Program.cs
+++ Core_gRPC_UnaryTemplate_Client/Program.cs
@@ -13,7 +13,7 @@ namespace Core_gRPC_UnaryTemplate_Client
             {
                 var channel = GrpcChannel.ForAddress("https://localhost:5001");
                 var messageClient = new message.messageClient(channel);
-                Console.WriteLine("Please Enter Message.");
+                Console.WriteLine("Please Enter Message .");
                 var messageResponse = await messageClient.GetMessageAsync(new MessageRequest { Message = Console.ReadLine() });
                 Console.WriteLine($"Response  : {messageResponse.Message}");
             }
